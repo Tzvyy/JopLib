c6b3c36b5b78a9d39dbf5c36e23cc7133ab20757
Tzvyy <tzvyy@users.noreply.github.com>
2026-04-16 11:54:43 -0300

    Make GUI taller: 850x650 default size

--- Library.lua
+++ Library.lua
@@ -408,7 +408,7 @@ function Library:CreateWindow(options)
     local center = options.Center ~= false
     local autoShow = options.AutoShow ~= false
     local windowWidth = options.Width or 850
-    local windowHeight = options.Height or 550
+    local windowHeight = options.Height or 650
     local tabPadding = options.TabPadding or 8
 
     if self.ScreenGui then
